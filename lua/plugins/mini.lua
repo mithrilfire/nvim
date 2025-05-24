fe4d065690974ea9e3b083abfc68c884cf2f2041
Fatih Calisir <27806840+mithrilfire@users.noreply.github.com>
2025-05-24 17:12:55 +0300

    feat: use mini.diff for git modifications

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,10 +3,24 @@ return {
     "echasnovski/mini.nvim",
     version = false,
     config = function()
-      local statusline = require("mini.statusline").setup(
+      require("mini.statusline").setup(
         { use_icons = true }
       )
       require("mini.notify").setup()
+      require("mini.diff").setup({
+        -- Options for how hunks are visualized
+        view = {
+          -- Visualization style. Possible values are 'sign' and 'number'.
+          -- Default: 'number' if line numbers are enabled, 'sign' otherwise.
+          style = 'sign',
+
+          -- Signs used for hunks with 'sign' view
+          signs = { add = '┃', change = '┃', delete = '┃' },
+
+          -- Priority of used visualization extmarks
+          priority = 1,
+        },
+      })
     end
   },
 }
