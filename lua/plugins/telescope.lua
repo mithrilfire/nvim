7413a795948d9803495c4a7caac1b366a4ac113e
Fatih Calisir <27806840+mithrilfire@users.noreply.github.com>
2025-06-18 12:10:22 +0300

    feat: add live grep keymap to telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -16,8 +16,12 @@ return {
 
       require('telescope').load_extension('fzf')
 
-      vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags)
-      vim.keymap.set('n', '<leader>fd', require('telescope.builtin').find_files)
+      local builtin = require('telescope.builtin')
+
+      vim.keymap.set('n', '<leader>fd', builtin.find_files, { desc = 'Telescope find files' })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
       vim.keymap.set('n', '<leader>en', function()
         require('telescope.builtin').find_files({
           cwd = vim.fn.stdpath("config")
